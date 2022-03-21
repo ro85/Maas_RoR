7150fcb815d8739ae09b8bf04d6706b91f70a6d2
Rodrigo Villanueva <r_villanueva18@hotmail.com>
2022-03-21 10:28:21 -0300

    Modal create contract y Readme.md

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 Contract.destroy_all
 User.destroy_all
 
-
-admin = User.new(name:"Rodrigo", admin:true, email:"admin@mail.com", password:"123456", dev: false)
+admin = User.new(name:"Admin", admin:true, email:"admin@mail.com", password:"123456", dev: false)
 admin.colour = admin.select_colour
 admin.save!
 p admin
 
-ernesto = User.new(name:"Ernesto", email:"ernesto@mail.com", password:"123456", dev: true)
-ernesto.colour = "#C65314"
-ernesto.save!
-p ernesto
+dev1 = User.new(name:"Developer", email:"dev@mail.com", password:"123456", dev: true)
+dev1.colour = "#C65314"
+dev1.save!
+p dev1
 
-barbara = User.new(name:"Bárbara", email:"barbara@mail.com", password:"123456", dev: true)
-barbara.colour = "#89133E"
-barbara.save!
-p barbara
+dev2 = User.new(name:"Developer 2", email:"dev2@mail.com", password:"123456", dev: true)
+dev2.colour = "#89133E"
+dev2.save!
+p dev2
 
-benjamin = User.new(name:"Benjamín", email:"benjamin@mail.com", password:"123456", dev: true)
-benjamin.colour = "#485856"
-benjamin.save!
-p benjamin
+dev3 = User.new(name:"Developer 3", email:"dev3@mail.com", password:"123456", dev: true)
+dev3.colour = "#485856"
+dev3.save!
+p dev3
